b8378c98dcf82fff5ca61728de7441e87f00c43b
Mobin <mamtn011@gmail.com>
2024-10-21 12:50:51 +0600

    created library finctions for api call

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -1,5 +1,48 @@
+const api = "https://gutendex.com/books/";
+
+// get books
 export const getBooks = async () => {
-  const response = await fetch("https://gutendex.com/books/");
+  const response = await fetch(api);
+  if (!response.ok) {
+    throw new Error(`Error in fetching books.`);
+  }
+  const data = await response.json();
+  return data;
+};
+
+// get books by id
+export const getBookById = async (id) => {
+  const response = await fetch(`${api}?ids=${id}`);
+  if (!response.ok) {
+    throw new Error(`Error in fetching books.`);
+  }
+  const data = await response.json();
+  return data;
+};
+// get books by page number
+export const getBooksByPage = async (pageNum) => {
+  const response = await fetch(`${api}?page=${pageNum}`);
+  if (!response.ok) {
+    throw new Error(`Error in fetching books.`);
+  }
+  const data = await response.json();
+  return data;
+};
+
+// get books by genre/topic
+export const getBooksByGenre = async (genre) => {
+  const response = await fetch(`${api}?topic=${genre}`);
+  if (!response.ok) {
+    throw new Error(`Error in fetching books.`);
+  }
+  const data = await response.json();
+  return data;
+};
+
+// get books by genre/topic
+export const getBooksByTitle = async (title) => {
+  const modifiedTitle = title.trim().split(" ").join("%20");
+  const response = await fetch(`${api}?search=${modifiedTitle}`);
   if (!response.ok) {
     throw new Error(`Error in fetching books.`);
   }
